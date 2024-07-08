ca4cb664b06e2bf58de1b0b9832be27634b95a54
thapelo <thapelowmakhanya@gmail.com>
2024-07-08 13:40:43 +0200

    Added Jpa an H2

--- src/main/java/com/goosenvi/studentapi/model/StudentInfo.java
+++ src/main/java/com/goosenvi/studentapi/model/StudentInfo.java
@@ -1,14 +1,31 @@
 package com.goosenvi.studentapi.model;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 import java.util.StringJoiner;
 
+@Entity
+@Table(name = "Students")
 public class StudentInfo {
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Id
+    @Column(name = "PK_ID")
+    private Long id;
+    @Column(name = "STUDENT_ID")
     private Long studentId;
+    @Column(name = "FIRSTNAME")
     private String firstName;
+    @Column(name = "LASTNAME")
     private String lastName;
+    @Column(name = "EMAIL")
     private String email;
+    @Column(name = "PROGRAMME")
     private String programme;
+
+    @ElementCollection
+    @CollectionTable(name = "Student_Courses", joinColumns = @JoinColumn(name = "STUDENT_ID"))
+    @Column(name = "COURSES")
     private List<String> courses;
 
     public StudentInfo(Long studentId, String firstName, String lastName, String email, List<String> courses, String programme) {
@@ -18,6 +35,18 @@ public class StudentInfo {
         this.email = email;
         this.courses = courses;
         this.programme = programme;
+    }
+
+    public StudentInfo() {
+        super();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public Long getStudentId() {
